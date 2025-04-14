ae3f8f66a5ce156dcf760d46513482563265dcb9
moonyongjun <yongjun1226@naver.com>
2025-04-14 15:17:47 +0900

    Create list.py

---
+++ list.py
@@ -0,0 +1,29 @@
+import random
+import time
+
+songs = ["a노래","b노래","c노래","d노래"]
+print(songs)
+print(songs[2])
+
+for song in songs :
+   print(songs)
+
+  print("AI야 노래 한 곡만 추천해줘")
+  print("""네""")
+
+  #여기서 ai가 들어야죠
+ ai_song = random.choice(songs)
+dd = ["두",두","두","두둥"]
+for d in dd
+    print(d)
+    time.sleep(1)
+  prin(f"내가  추천하는 ai song 이거야""")
+    
+#리스트를 쓰는 이유
+song1 = "a노래"
+song2 = "b노래"
+song3 = "c노래"
+
+print(song1)
+print(song2)
+print(song3)
